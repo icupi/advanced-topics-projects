5801816e70025be13caadd855e7502b1a8084dd3
Ryan Tchoumi Tchouga <rtchoumitchouga@gmail.com>
2025-10-13 22:54:40 -0400

    Update hello.c
    I added the goodbye feature

--- src/hello.c
+++ src/hello.c
@@ -6,6 +6,7 @@ int main(){
     	printf("Enter your name: "); //asks for user input
     	scanf("%99s", name); //gets user input
     	printf("Hello, %s!\n", name); //prints Hello and the name
+		printf("Goodbye, %s\n", user); //prints a goodbye message
 	return 0;
 }
 
